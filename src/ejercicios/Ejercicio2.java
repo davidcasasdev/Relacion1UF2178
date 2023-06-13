c08fe511c55e97b6681683cabf112a2919cc931d
David <David@DESKTOP-QHMC14O>
2023-06-13 14:14:14 +0200

    Ejercicio 2 añadido

---
+++ src/ejercicios/Ejercicio2.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// Lee un nombre y muetralo con el texto Buenos días, texto introducido
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Introduce tu nombre: ");
+		String nombre = teclado.nextLine();
+		
+		System.out.printf("Buenos días, %s\n",nombre);
+	}
+
+}
