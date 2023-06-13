4ad435a1d701dae1ec4369016eacffb7187aeb6c
David <David@DESKTOP-QHMC14O>
2023-06-13 14:06:44 +0200

    Añadido el ejercicio 1
    se ha modificado para solucionar el primer ejercicio

--- src/ejercicios/Ejercicio1.java
+++ src/ejercicios/Ejercicio1.java
@@ -1,9 +1,21 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
+		// Pide 2 numeros por teclado y los muestra en pantalla
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Introduce un número:");
+		double num1 = teclado.nextDouble();
+		
+		System.out.println("Introduce otro número:");
+		double num2 = teclado.nextDouble();
+		
+		System.out.println("El numero 1 es "+num1
+				+ " y el número 2 es "+ num2);
 
 	}
 
